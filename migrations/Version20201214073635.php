037f4530c0cba0f3290d4bcbd400f41b093ea95b
romdev <uacyberdevgroup@gmail.com>
2020-12-14 09:37:23 +0200

    Added basic settup

---
+++ migrations/Version20201214073635.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20201214073635 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE record (id INT AUTO_INCREMENT NOT NULL, ident VARCHAR(32) NOT NULL, value VARCHAR(255) NOT NULL, version INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('DROP TABLE data');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE data (ident VARCHAR(32) CHARACTER SET utf8 NOT NULL COLLATE `utf8_general_ci`, value VARCHAR(255) CHARACTER SET utf8 NOT NULL COLLATE `utf8_general_ci`, version INT UNSIGNED NOT NULL, UNIQUE INDEX ident (ident)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = MyISAM COMMENT = \'\' ');
+        $this->addSql('DROP TABLE record');
+    }
+}
---
+++ src/Entity/Record.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RecordRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=RecordRepository::class)
+ */
+class Record
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=32)
+     */
+    private $ident;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $value;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $version;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getIdent(): ?string
+    {
+        return $this->ident;
+    }
+
+    public function setIdent(string $ident): self
+    {
+        $this->ident = $ident;
+
+        return $this;
+    }
+
+    public function getValue(): ?string
+    {
+        return $this->value;
+    }
+
+    public function setValue(string $value): self
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+
+    public function getVersion(): ?int
+    {
+        return $this->version;
+    }
+
+    public function setVersion(int $version): self
+    {
+        $this->version = $version;
+
+        return $this;
+    }
+}
